4551bfed642a50e4eec80f33d7e6e408cfa9fb66
MykolaKomar <mykola.komar19@gmail.com>
2023-12-20 13:40:05 +0200

    don	 all

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,14 @@
-import '../../script/test'
-
-console.log('container')
+// через document.querySelector звертаємось до селектора .form__button
+// через onclick отримуємо форму в ручну
+document.querySelector('.form__button').onclick = () => {
+  // заносимо її в змінну form череp document.getElementById
+  const form = document.getElementById('form')
+  // якщо валідність коректна відправляємо форму
+  if (form.reportValdity()) {
+    form.submit()
+  }
+  //   якщо валідність не коректна очищаємо всі поля
+  else {
+    form.reset()
+  }
+}
